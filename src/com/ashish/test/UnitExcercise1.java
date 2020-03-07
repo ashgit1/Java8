6c034045753af8cb72d0a67cac6bf2b2aba373ed
Ashishkumar Gupta <akumar9@walmartlabs.com>
2020-03-07 20:05:31 +0530

    Lambda Exercise Question

---
+++ src/com/ashish/test/UnitExcercise1.java
@@ -0,0 +1,28 @@
+package com.ashish.test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class UnitExcercise1 {
+
+	public static void main(String[] args) {
+		
+		List<Person> people = Arrays.asList(
+				new Person("Ashish", "Gupta", 30),
+				new Person("Arpitha", "Kumar", 24),
+				new Person("Dikshitha", "Gonzalvez", 18),
+				new Person("Kashi", "Tiwari", 54),
+				new Person("Sunita", "Kaur", 50),
+				new Person("Neha", "Kumari", 50)
+				);
+
+		// Step 1: Sort List by last name
+		
+		// Step 2: create a method that prints all elements of the list
+		
+		// Step 3: create a method that prints all last names beginning with K
+		
+		
+	}
+
+}
