91d7dd846890ef8744cd368096bc7a57b003af40
Ashishkumar Gupta <akumar9@walmartlabs.com>
2020-03-08 22:06:34 +0530

    Introduction To Streams

---
+++ src/com/ashish/test/StreamExample1.java
@@ -0,0 +1,17 @@
+package com.ashish.test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamExample1 {
+
+	public static void main(String[] args) {
+		List<Person> people = Arrays.asList(new Person("Ashish", "Gupta", 30), new Person("Arpitha", "Kumar", 24),
+				new Person("Dikshitha", "Gonzalvez", 18), new Person("Kashi", "Tiwari", 54),
+				new Person("Sunita", "Kaur", 50), new Person("Neha", "Kumari", 50));
+
+		people.stream()
+		.filter(p -> p.getFirstName().startsWith("A"))
+		.forEach(p -> System.out.println(p.getLastName()));
+	}
+}
